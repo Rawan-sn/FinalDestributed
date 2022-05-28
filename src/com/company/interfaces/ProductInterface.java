24d60817e4ea8cc32253f9659a7e708652e9f25b
rawan_sn <Rawan.sn99@gmail.com>
2022-05-29 01:20:46 +0300

    product

---
+++ src/com/company/interfaces/ProductInterface.java
@@ -0,0 +1,9 @@
+package com.company.interfaces;
+
+import com.company.product;
+
+import java.util.List;
+
+public interface ProductInterface {
+   public List<product> getProducts();
+}
--- src/com/company/product.java
+++ src/com/company/product.java
@@ -1,37 +1,85 @@
 package com.company;
 
+//import org.hibernate.annotations.GenericGenerator;
+//import org.hibernate.validator.constraints.NotEmpty;
 public class product {
-    private String name;
-    private String status;
-    private int price;
+    //    @Id
+//    //@GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(generator="system-uuid")
+//    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String productId;
+    private String productName;
+    private String productCategory;
+    private String productDescription;
+    private double productPrice;
+    private int unitInStock;
 
-    public product(String name, String status, int price) {
-        this.name = name;
-        this.status = status;
-        this.price = price;
+    public product(String productId, String productName, String productCategory, String productDescription, double productPrice, String productStatus, int unitInStock) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productCategory = productCategory;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.unitInStock = unitInStock;
     }
 
-    public String getName() {
-        return name;
+    public String getProductId() {
+        return productId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setProductId(String productId) {
+        this.productId = productId;
     }
 
-    public String getStatus() {
-        return status;
+    public String getProductName() {
+        return productName;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setProductName(String productName) {
+        this.productName = productName;
     }
 
-    public int getPrice() {
-        return price;
+    public String getProductCategory() {
+        return productCategory;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getUnitInStock() {
+        return unitInStock;
+    }
+
+    public void setUnitInStock(int unitInStock) {
+        this.unitInStock = unitInStock;
+    }
+
+    @Override
+    public String toString() {
+        return "product{" +
+                "productId='" + productId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", productCategory='" + productCategory + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", productPrice=" + productPrice +
+                ", unitInStock=" + unitInStock +
+                '}';
     }
 }
